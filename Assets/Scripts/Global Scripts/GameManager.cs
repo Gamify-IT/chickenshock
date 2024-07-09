83eb45ca15be0df4864b587307e868bba307bbc3
Duyguoe <duyguoezden96@gmail.com>
2024-07-09 15:23:29 +0200

    Updated overview

--- Assets/Scripts/Global Scripts/GameManager.cs
+++ Assets/Scripts/Global Scripts/GameManager.cs
@@ -37,6 +37,8 @@ public class GameManager : MonoBehaviour
     private int wrongKillsCount;
     private int shotCount;
     private int points;
+    private int score;
+    private int rewards;
     private List<RoundResult> correctAnsweredQuestions;
     private List<RoundResult> wrongAnsweredQuestions;
     #endregion
@@ -86,6 +88,8 @@ public class GameManager : MonoBehaviour
         this.points = 0;
         this.wrongAnsweredQuestions = new List<RoundResult>();
         this.correctAnsweredQuestions = new List<RoundResult>();
+        this.score = 0;
+        this.rewards = 0;
         this.pointOverlay = GameObject.FindGameObjectWithTag("Point Overlay");
         this.pointOverlay.GetComponent<TMPro.TextMeshProUGUI>().text = this.points.ToString();
         EndScreen.errorText = "";
@@ -374,7 +378,7 @@ public class GameManager : MonoBehaviour
     /// <param name="uri"></param>
     private IEnumerator PostRequest(String uri)
     {
-        GameResult round = new GameResult(questionCount,timeLimit,finishedInSeconds,correctKillsCount,wrongKillsCount,correctKillsCount + wrongKillsCount, shotCount,points,correctAnsweredQuestions,wrongAnsweredQuestions, configurationAsUUID);
+        GameResult round = new GameResult(questionCount,timeLimit,finishedInSeconds,correctKillsCount,wrongKillsCount,correctKillsCount + wrongKillsCount, shotCount,points,correctAnsweredQuestions,wrongAnsweredQuestions, configurationAsUUID, score, rewards);
         string jsonRound = JsonUtility.ToJson(round);
         byte[] jsonToSend = new UTF8Encoding().GetBytes(jsonRound);
 
@@ -401,8 +405,6 @@ public class GameManager : MonoBehaviour
                     Debug.Log(uri + ":\nReceived: " + postRequest.downloadHandler.text);
                     string jsonResponse = postRequest.downloadHandler.text;
                     receivedGameResult = JsonUtility.FromJson<GameResult>(jsonResponse);
-                    Debug.Log(receivedGameResult);
-
 
                     break;
             }
